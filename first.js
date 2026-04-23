68fd2aa2e413380ffa43daf50126370249a6260d
sahnur shifat <sahnurshifat9@gmail.com>
2026-04-23 11:18:38 +0600

    notes taken

--- first.js
+++ first.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+console.log("Shanur Alam Shifat");
+console.log("age= 24")
+fullName = "Shanur Alam Shifat";
+Age = 24;
+Day = 01;
+console.log(fullName);
+console.log(Age);
+console.log(Day);
+
+/** Variables are case sensitive 
+    we will use let, const to declare the variable.
+    var use korbo  na cz var diye variable redeclare 
+    kora jay.
+
+**/
